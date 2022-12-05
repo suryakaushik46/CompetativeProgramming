d15a4850ac1ff4a1aae325e79621d20a8eec73a8
surya kaushik <62637092+suryakaushik46@users.noreply.github.com>
2022-12-05 16:03:05 +0000

    day 17

---
+++ .ccls-cache/@home@runner@A-Soft-Drinking/Maths: (10*5=50)@Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin>>n
+   if(n&1){
+     cout<<"Ehab"<<endl;
+   }else{
+     cout<<"Mahmoud"<<endl;
+   }
+   return 0;
+}
---
+++ Maths: (10*5=50)/LCM Problem.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   while(t--){
+      int a,b;
+      cin>>a>>b;
+       if(a*2<=b){
+          cout<<a<<" "<<a*2<<endl;
+       }elsE{
+         cout<<-1<<" "<<-1<<endl;
+       }
+      }
+   }
+   return 0;
+}
